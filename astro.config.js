ab42e112afba32c54906e7e84644f47dd08fc257
Lucas L <18102488+smixqse@users.noreply.github.com>
2023-01-25 00:05:40 +0000

    nao liga pra esse commit nao por favor

---
+++ astro.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'astro/config';
+import svelte from '@astrojs/svelte';
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+  // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
+  // pages: './src/pages', // Path to Astro components, pages, and data
+  // dist: './dist',       // When running `astro build`, path to final static output
+  // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
+  site: 'https://smixqse.top',
+  // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+  integrations: [svelte(), sitemap(), tailwind()]
+});
